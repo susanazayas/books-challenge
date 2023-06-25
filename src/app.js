383e65943aae2ccf986164966bf960c3e402be33
susanazayas <susyzayas@gmail.com>
2023-06-25 13:48:10 -0300

    subi cambio

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const mainRouter = require('./routes/main');
 const methodOverride= require("method-override")
 const session=require("express-session")
-const cookieparser=require("cookie-parser")
+const cookieParser = require("cookie-parser")
 const app = express();
 const path=require("path");
 const cookieCheck = require('./middlewares/cookieCheck');
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("method"))
+app.use(methodOverride("method"));
 app.use(express.json());
 app.use(session({ secret:"bookChallenge",
 resave: false, 
 saveUninitialized: true 
 })) 
-app.use(cookieparser());
+app.use(cookieParser());
 app.use(cookieCheck)
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
